970c005785f54355d339b3b8457e51cf7a44d2b4
Izaac Eduardo Soares <izaaceduardosoares24@gmail.com>
2025-04-04 21:08:00 -0300

    desafio pt1

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velecidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples
+    console.log('Vc ultrapassou a velocidade permitida! MULTADO!')
+}
+console.log('Dirija sempre usando sinto de segurança')
